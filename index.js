ecdb0d5b3c3df346c0e66833795b1a5b8fefba75
Mike Amundsen <mamund@yahoo.com>
2018-06-10 22:58:01 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodei --harmony
 
 var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
+var request = require('superagent');
 
 program
   .arguments("<file>")
@@ -12,7 +13,15 @@ program
     co(function *() {
       var username = yield prompt("username:");
       var password = yield prompt.password("password:");
-      console.log("user: %s pass: %s file: %s", username, password, file);
+      request
+        .post('https://api.bitbucket.org/2.0/snippets/')
+        .auth(username, password)
+        .attach('file', file)
+        .set('accept', 'application/json')
+        .end(function(err,res) {
+          var link = res.body.links.html.href;
+          console.log('snippet created: %s', link);
+        });
     });  
   })
   .parse(process.argv);
